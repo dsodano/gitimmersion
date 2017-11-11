c7c1e337c8a1c543b5c2088a73dca5e78f3fb012
Dominic Sodano <sodanodominic@gmail.com>
2017-11-11 21:45:19 +0000

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
-# Default is "World"
-# Author: Dominic Sodano (dsodano@uncc.edu)
+require 'greeter'
+
+# Default is World
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
